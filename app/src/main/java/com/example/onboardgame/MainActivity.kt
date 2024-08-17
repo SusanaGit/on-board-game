f1b93b0521006401f3e77c07bd039f6455ecc5e9
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-08-17 19:38:33 +0200

    starting app

---
+++ app/src/main/java/com/example/onboardgame/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.onboardgame
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.onboardgame.ui.theme.OnBoardGameTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OnBoardGameTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {}
+            }
+        }
+    }
+}
