8ab6da65d2d1c11b6e154495737f86e23d6dcf05
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-08-18 21:40:12 +0200

    show OnBoardGame

--- app/src/main/java/com/example/onboardgame/MainActivity.kt
+++ app/src/main/java/com/example/onboardgame/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.onboardgame.ui.theme.OnBoardGameTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +22,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             OnBoardGameTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {}
+                ) {
+                    OnBoardGame()
+                }
             }
         }
     }
@@ -34,11 +37,21 @@ class MainActivity : ComponentActivity() {
 
         val direction = remember { mutableStateOf("North")}
 
-        Column {
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
             Text(text = "Treasures: ${treasures.value}")
             Text(text = "Direction: ${direction.value}")
         }
-
     }
 
+    @Preview(showBackground = true)
+    @Composable
+    fun OnBoardGamePreview(){
+        OnBoardGame()
+    }
 }
