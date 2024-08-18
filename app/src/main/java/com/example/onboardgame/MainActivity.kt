bfa595442642a9b67d0d3aa4c72cc4aec4e40c77
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-08-18 23:03:40 +0200

    add arrows and random treasure

--- app/src/main/java/com/example/onboardgame/MainActivity.kt
+++ app/src/main/java/com/example/onboardgame/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.onboardgame.ui.theme.OnBoardGameTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +51,35 @@ class MainActivity : ComponentActivity() {
         ){
             Text(text = "Treasures: ${treasures.value}")
             Text(text = "Direction: ${direction.value}")
+
+            Row() {
+
+                Button(
+                    onClick = {
+                        direction.value = "Left"
+                        if (Random.nextBoolean()) {
+                            treasures.value += 1
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Go Left",
+                    )
+                }
+
+                Button(onClick = {
+                    direction.value = "Right"
+                    if(Random.nextBoolean()){
+                        treasures.value += 1
+                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowForward,
+                        contentDescription = "Go Right",
+                    )
+                }
+            }
         }
     }
 
